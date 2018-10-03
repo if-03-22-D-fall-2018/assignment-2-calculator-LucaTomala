76807b3afc68693b43328c2944e0369f36c0cea5
LucaTomala <luca.tomala@gmail.com>
2018-10-03 17:40:41 +0200

    few changes in the code

--- calculator.c
+++ calculator.c
@@ -1,55 +1,129 @@
+lol/*----------------------------------------------------------
+ *				HTBLA-Leonding / Class: 2DHIF
+ * ---------------------------------------------------------
+ * Exercise Number: 2
+ * Title:			Calculator
+ * Author:			Luca Tomala 
+ * ----------------------------------------------------------
+ * Description:
+ *  A programm which calulates 2 digits
+ * ----------------------------------------------------------
+*/
+
 #include <stdio.h>
-printf("Choose on of the following operations:\n", );
-printf("Add (1)\n", );
-printf("Subtract (2)\n", );
-printf("Multiply (3)\n", );
-printf("Divide (4)\n", );
-printf("Stop Programm (-1)\n", );
-printf("Enter your choise: ", );
-int operation;
-scanf("%s\n", &operation);
+#include <float.h>
 
-if (operation == -1) {
-  printf("Programm canceld by user\n", );
-}
-else if (operation == 1) {
-  Get_first_operand();
-  Get_second_operand();
-  double result = firstOperand + secondOperand;
-  printf("%fThe result is \n",&result );
-}
-else if (operation == 2) {
-  Get_first_operand();
-  Get_second_operand();
-  double result = firstOperand- secondOperand;
-  printf("%fThe result is \n",&result );
-}
-else if (operation == 3) {
-  Get_first_operand();
-  Get_second_operand();
-  double result = firstOperand * secondOperand;
-  printf("%fThe result is \n",&result );
-}
-else if (operation == 4) {
-  Get_first_operand();
-  Get_second_operand();
-  if (operand == 0) {
-    printf("The operand \n", );
-  }
+void getNumbers(double* firstOperand, double* secondOperand);
+int calculate(int type, double firstNumber, double secondNumber)
+void menu(int selection);
 
-  printf("%fThe result is \n",&result );
+int main(int argc, const char *argv[])
+{
+    int selection = -1;
+    double first = 0;
+    double second = 0;
+    do
+    {
+        menu(&selection);
+
+        if (selection != -1)
+        {
+
+            getNumbers(&first, &second);
+            int calculation_status = calculate(&selection, &first, &second);
+            if (calculation_status == -1)
+            {
+                printf("Number overflow\n");
+            }
+
+            else if (calculation_status == -2)
+            {
+                printf("Number underflow\n");
+            }
+        }
+    }
+    while(selection != -1);
+    return 0;
 }
 
+int calculate(int type, double firstNumber, double secondNumber)
+{
+    double calculationResult;
+    switch (type)
+    {
+        case 1:
+            if (firstOperand + secondOperand > DBL_MAX) {
+                return -1;
+            }
+            else if (firstOperand + secondOperand < -DBL_MAX) {
 
+                return -2;
+            }
+            calculationResult = firstOperand + secondOperand;
+            break;
+        case 2:
+            if (firstOperand - secondOperand > DBL_MAX) {
 
+                return -1;
+            }
+            else if (firstOperand - secondOperand < -DBL_MAX) {
 
-double Get_first_operand(){
-  double firstOperand;
-  printf("Please enter your first operand: ", );
-  scanf("%f\n",&firstOperand );
+                 return -2;
+            }
+            calculationResult = firstOperand - secondOperand;
+            break;
+        case 3:
+            if (firstOperand * secondOperand > DBL_MAX) {
+
+                return -1;
+            }
+            else if (firstOperand * secondOperand < -DBL_MAX) {
+
+                 return -2;
+            }
+            calculationResult = firstOperand * secondOperand;
+            break;
+        case 4:
+            if (firstOperand / secondOperand > DBL_MAX) {
+
+                return -1;
+            }
+            else if (firstOperand / secondOperand < -DBL_MAX) {
+                return -2;
+            }
+            calculationResult = firstOperand / secondOperand;
+            break;
+    }
+
+    printf("Calculation Result: %lf\n", calculationResult);
+    return 0;
 }
-double Get_second_operand(){
-  double secondOperand;
-  printf("Please enter your second operand:", );
-  scanf("%f\n",&secondOperand );
+
+void getNumbers(double* firstOperand, double* secondOperand)
+{
+    printf("Please enter the first operand: ");
+    scanf("%lf", firstOperand);
+
+    printf("Please enter the second operand: ");
+    scanf("%lf", secondOperand);
+}
+
+void menu(int selection)
+{
+    printf("Choose one of the following operations:\n");
+    printf("    Add (1)\n");
+    printf("    Subtract (2)\n");
+    printf("    Multiply (3)\n");
+    printf("    Divide (4)\n");
+    printf("Stop program (-1)\n");
+
+    do
+    {
+        if (!(input >= -1 && input <= 4))
+        {
+            printf("Input not allowed, please try again\n");
+        }
+        printf("Enter your choice: ");
+        scanf("%d", selection);
+    } while (!(input >= -1 && input <= 4));
 }
